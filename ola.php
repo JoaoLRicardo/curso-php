aca2be6972c271ef1da58be7fb95f44165346599
JoaoLRicardo <joao.luiz.ric@gmail.com>
2022-09-24 01:51:09 -0300

    Update ola.php

--- ola.php
+++ ola.php
@@ -5,11 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Primeiro exemplo em PHP</title>
+    <style>
+        h2 {
+            color: #80a2ff;
+            text-shadow: 1px 1px 1px black;
+        }
+    </style>
 </head>
 <body>
     <h1>Testando PHP</h1>
     <?php
-        echo "Olá, Mundo!";
+        echo "<h2>Olá, Mundo!</h2>";
     ?>
 </body>
 </html>
