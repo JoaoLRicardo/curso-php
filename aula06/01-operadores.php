412b5368132491ea69164ee4a88b5eb278bc675d
JoaoLRicardo <joao.luiz.ric@gmail.com>
2022-09-26 01:32:30 -0300

    Update

--- aula06/01-operadores.php
+++ aula06/01-operadores.php
@@ -11,8 +11,12 @@
     <div>
         <?php
             $preco = $_GET["p"];
-            echo "O preço do produto é R$ $preco";
-
+            echo "O preço do produto é R$ ". number_format($preco, 2, ",", ".");
+            $preco += ($preco * 10/100);
+            echo "<br/>E o novo preço com 10% de aumento será R$ ". number_format($preco, 2, ",", ".");
+            $preco = $_GET["p"];
+            $preco -= $preco * 10/100;
+            echo "<br/>E o novo preço com 10% de desconto será R$ ". number_format($preco, 2, ",", ".");
         ?>
 
     </div>
